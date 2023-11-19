9a891f98bf2f519089c28f15685266b3c419d9d5
Venkynarla <108213954+Venkynarla@users.noreply.github.com>
2023-11-19 19:22:26 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -152,15 +152,3 @@ resource "aws_instance" "deploy-node" {
     Name        = "deploy-node"
   }
 }
-resource "aws_key_pair" "task_key" {
-        key_name        =       "task_key"
-        public_key      =       tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-        algorithm       =       "RSA"
-        rsa_bits        =       4096
-}
-resource "local_file" "task_key" {
-        content  =      tls_private_key.rsa.private_key_pem
-        filename =      "task_key"
-}
